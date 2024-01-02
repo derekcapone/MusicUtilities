365c3e037516aa0978d6d8a17892d65f95eb9e32
Derek Capone <derekcapone7@gmail.com>
2024-01-02 11:58:05 -0400

    Fixes circular imports and simplifies some modules into one module

--- chord_finder.py
+++ chord_finder.py
@@ -1,6 +1,25 @@
+from note import Note
+from interval import Interval
+
+
 def find_chord(notes):
     print(f"Finding chords for {notes}")
 
 
+def find_simple_interval(root_note, next_note):
+    """
+    Takes 2 Note objects and finds the interval between them irrespective of octave
+    :param root_note: Root note to use as base for interval calculation
+    :param next_note: Note to find the interval from root note
+    :return: Interval enum
+    """
+    print(f"Finding interval for {next_note} compared to {root_note}")
+    if not isinstance(root_note, Note) or not isinstance(next_note, Note):
+        raise Exception("root_note or next_note are not of type Note")
+
+
 if __name__ == '__main__':
-    print("Hello")
+    note1 = Note("A4")
+    print(note1.note_string)
+    note2 = Note("A#4/Bb4")
+    print(note2.note_string)
---
+++ interval.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Interval(Enum):
+    """
+    Enum to describe the intervals with respect to half step note changes
+    """
+    UNISON = 0
+    MINOR_SECOND = 1
+    MAJOR_SECOND = 2
+    MINOR_THIRD = 3
+    MAJOR_THIRD = 4
+    PERFECT_FOURTH = 5
+    TRITONE = 6
+    PERFECT_FIFTH = 7
+    MINOR_SIXTH = 8
+    MAJOR_SIXTH = 9
+    MINOR_SEVENTH = 10
+    MAJOR_SEVENTH = 11
